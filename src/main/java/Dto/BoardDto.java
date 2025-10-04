7fc96495d519f0ef2b7d86bff1a57e49a5a42101
naoufal <naoufaljrh2000@gmail.com>
2025-10-04 13:39:30 +0100

    created dtos, first version

---
+++ src/main/java/Dto/BoardDto.java
@@ -0,0 +1,18 @@
+package Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BoardDto {
+    private Long id;
+    private List<ColumnDto> columns;
+}
---
+++ src/main/java/Dto/ColumnDto.java
@@ -0,0 +1,17 @@
+package Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ColumnDto {
+    private Long id;
+    private List<CardDto> cards;
+}
