095ab1416fe2d3d35cc0592713832df375b6ca90
naoufal <naoufaljrh2000@gmail.com>
2025-10-04 13:35:56 +0100

    first commit

---
+++ src/main/java/com/demo/trellolite/TrelloLiteApplication.java
@@ -0,0 +1,13 @@
+package com.demo.trellolite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloLiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloLiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/trellolite/TrelloLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.trellolite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloLiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
