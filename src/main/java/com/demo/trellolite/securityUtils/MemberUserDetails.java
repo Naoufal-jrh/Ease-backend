98c3fe0961f8486ea7c816000bdc3b52ec277499
naoufal <naoufaljrh2000@gmail.com>
2025-10-21 12:23:33 +0100

    User details and user details service classes implemented to define the authenticated user entity and the unique identifier of the user which is the email in this case

---
+++ src/main/java/com/demo/trellolite/securityUtils/MemberUserDetails.java
@@ -0,0 +1,57 @@
+package com.demo.trellolite.securityUtils;
+
+import com.demo.trellolite.Entity.Member;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+public class MemberUserDetails implements UserDetails {
+    private Member member;
+    private String userName;
+    private String password;
+
+    public MemberUserDetails() {
+        this.userName = member.getEmail();
+        this.password = member.getPasswordHash();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/demo/trellolite/securityUtils/MemberUserDetailsEmailService.java
@@ -0,0 +1,20 @@
+package com.demo.trellolite.securityUtils;
+
+import com.demo.trellolite.Repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class MemberUserDetailsEmailService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return memberRepository.findByEmail(email);
+    }
+}
